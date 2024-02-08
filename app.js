3429585f69798c59a43fc86d19b5c9d32a19ec87
TanishqSingla <tanishq@tradeindia.com>
2024-02-08 15:15:27 +0530

    feat: Upgrade request interceptor to logger
    This change modifies the interceptor middleware to log response as well,
    since the interceptor logs the request as well as resposne it is renamed as
    logger

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { router as v1Router } from './routes/v1/routes.js';
 import expressBang from 'express-bang';
 import { errorHandler } from './middlewares/errorHandler.js';
-import { requestInterceptor } from './middlewares/interceptor.js';
+import { logger } from './middlewares/logger.js';
 
 export const app = express();
 
 app.use(expressBang());
 if(["development", "staging"].includes(process.env.NODE_ENV)) {
-	app.use(requestInterceptor);
+	app.use(logger)
 }
 
 app.use("/v1/", v1Router);
